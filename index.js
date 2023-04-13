136e04e9ca5059284861ca2340fc165bb6dc57a7
Ninos Dinkha <ninos.dinkha97@gmail.com>
2023-04-13 18:47:35 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((sum, side) => sum + side, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    const [a, b, c] = this.sides;
+    return a + b > c && b + c > a && c + a > b;
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    const [side1, side2, side3, side4] = this.sides;
+    return side1 === side2 && side2 === side3 && side3 === side4;
+  }
+
+  get area() {
+    const [side1, , ,] = this.sides;
+    return side1 * side1;
+  }
+}
